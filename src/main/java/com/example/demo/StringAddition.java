81028dfc1946aaff23055f7f5bdcea74248001ef
Ajay <ajaygchavda@gmail.com>
2024-09-09 23:10:22 +0530

    1. added support of one string to be converted as addition and return as digit

--- src/main/java/com/example/demo/StringAddition.java
+++ src/main/java/com/example/demo/StringAddition.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import org.springframework.util.StringUtils;
+
 public class StringAddition {
     public int add(String numbers ) {
-        return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        } else {
+            if(!numbers.contains(",")){
+                return Integer.parseInt(numbers);
+            }
+            return 0;
+        }
     }
 }
--- src/test/java/com/example/demo/StringAdditionTest.java
+++ src/test/java/com/example/demo/StringAdditionTest.java
@@ -10,4 +10,10 @@ public class StringAdditionTest {
         int actualAddition = new StringAddition().add("");
         Assertions.assertEquals(0,actualAddition);
     }
+
+    @Test
+    public void addNumbersWhenOneNumberGiven(){
+        int actualAddition = new StringAddition().add("1");
+        Assertions.assertEquals(1,actualAddition);
+    }
 }
